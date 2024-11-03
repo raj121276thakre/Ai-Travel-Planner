85257279bd6bbce2c6c9cb2aba67a3d6544f197b
raj121276thakre <raj121276thakre@gmail.com>
2024-11-03 20:00:18 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      
+      }}
+    >
+      <Text style={{
+        fontSize: 30,
+        fontFamily:'outfit-bold'
+      }}>Subscribe.</Text>
+    </View>
+  );
+}
